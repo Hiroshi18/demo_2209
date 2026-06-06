44be17d9e7d1d3fc5b26a575d71c16dd64f3d1a8
Hiroshi <lucas.hh@hotmail.com>
2026-06-06 11:38:08 -0300

    add new file

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.markdown("""
+            # This is a header
+            ## This is a sub header!
+            """)
+
+def get_select_box_data():
+
+    return pd.DataFrame({
+          'first column': list(range(1, 11)),
+          'second column': np.arange(10, 101, 10)
+        })
+
+df = get_select_box_data()
+
+option = st.selectbox('Select a line to filter', df['first column'])
+
+filtered_df = df[df['first column'] == option]
+
+st.write(filtered_df)
+
+if st.button('click me'):
+    # print is visible in the server output, not in the page
+    print('button clicked!')
+    st.write('I was clicked 🎉')
+    st.write('Further clicks are not visible but are executed')
+else:
+    st.write('I was not clicked 😞')
